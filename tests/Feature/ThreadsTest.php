e2722d8bb4b00946993f8d2aa774087691f0e42d
fishCakes <anthy.cook@gmail.com>
2019-12-26 04:10:27 +0000

    initial

---
+++ tests/Feature/ThreadsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ThreadsTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_a_user_can_browse_threads()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertStatus(200);
+    }
+}

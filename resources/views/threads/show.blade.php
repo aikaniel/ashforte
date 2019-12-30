f2dc6b628d4f9266c4c491eff6572a115071b267
Anthony <anthy.cook@gmail.com>
2019-12-30 01:40:21 +0000

    lesson 3 complete - display replies of a thread

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -6,9 +6,18 @@
         <div class="col-md-8">
             <article class="card">
                 <header class="card-header"><h3>{{$thread->title}}</h3></header>
-
                 <section class="card-body">
                     {{$thread->body}}
+                </section>
+                <section class="container-fluid">
+                    @foreach ($thread->replies as $reply)
+                    <article class="card">
+                        <header class="card-header"> <a href="#">{{ $reply->user->name }}</a> said {{ $reply->created_at->diffForHumans() }}...</header>
+                        <section class="card-body">
+                            {{$reply->body}}
+                        </section>
+                    </article>
+                    @endforeach
                 </section>
             </article>
         </div>
